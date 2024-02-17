edc47d73904eaa299fabd8aeb82fa9980d5386a0
KAVYA P <112717346+KavyaPremaseelan@users.noreply.github.com>
2024-02-17 23:36:24 +0530

    Add files via upload

---
+++ Enchanteur/javascript/script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navbar links
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
